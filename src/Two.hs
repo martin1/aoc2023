af2bc8fd6ad9eb21ab0d851e0b87490252b7fd3b
Martin Kurgi <martinkurgi@gmail.com>
2024-01-21 09:51:33 +0200

    wip

--- src/Two.hs
+++ src/Two.hs
@@ -1,5 +1,8 @@
 module Two where
 import Data.List.Split
+import Data.Bifunctor (Bifunctor(first))
+
+type GameData = [String]
 
 data Bag = Bag {
     red :: Int,
@@ -7,26 +10,28 @@ data Bag = Bag {
     green :: Int
 } deriving (Eq, Show)
 
-type GameData = [String]
-
 gamePrefix :: String
 gamePrefix = "Game "
 
 
 getGameData :: String -> (Int, [GameData])
-getGameData s = (gameId, games)
+getGameData s = (gameId, gameData)
     where
         splitResult = splitOn ": " s
         gameId = read $ drop (length gamePrefix) (head splitResult)
         games = splitOn "; " (last splitResult)
+        gameData = map (splitOn ", ") games
+
 
 -- getResult :: String -> Bag -> IO Int
 -- getResult s = 
 
 -- 3 blue, etc
-updateBag :: (Int, String) -> Bag -> Bag
-updateBag (n, suffix) bag
+updateBag :: String -> Bag -> Bag
+updateBag s bag
     | suffix == " red" = bag {red = n}
     | suffix == " green" = bag {green = n}
     | suffix == " blue" = bag {blue = n}
     | otherwise = bag
+    where 
+        (n, suffix) = first (\a -> read a :: Int) (break (== ' ') s)
