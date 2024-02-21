30e4c95a624c7a5582c129a17a6c07d6f3f9b99e
Martin Kurgi <martinkurgi@gmail.com>
2024-02-21 19:40:38 +0200

    Refactor getLineData function to use LineData data type

--- src/Three.hs
+++ src/Three.hs
@@ -1,15 +1,22 @@
-module Three where
+module Three() where
 
 import Data.Char (isDigit)
 import Data.List.Split (split)
 import Data.List.Split.Internals (whenElt)
+import Data.List (partition)
 
-type NumWithIndex = (Int, Int)
-type SymbolIndex = Int
 
-getLineData :: String -> [(String, Int)]
-getLineData s = [ (x, i) | (x, i) <- zip list indexes]
+data LineData = LineData {
+    nums :: [(String, Int)],
+    symbolsAt :: [Int]
+} deriving (Eq, Show)
+
+
+getLineData :: String -> LineData
+getLineData s = LineData numbers (map snd symbols)
     where
-        list = filter (not . null ) (split (whenElt (not . isDigit)) s)
+        list = filter (not . null) (split (whenElt (not . isDigit)) s)
         --indexes need to take into account the length of the strings
         indexes = scanl (\acc x -> acc + length x) 0 list
+        listIndexed = filter (\ (a, _) -> a /= ".") $ zip list indexes
+        (numbers, symbols) = partition (\(a, _) -> all isDigit a) listIndexed
