5618fb67318e57c48f201e8b5cfca613b67add5a
Martin Kurgi <martinkurgi@gmail.com>
2024-02-25 19:03:41 +0200

    Cleanup

--- src/Three.hs
+++ src/Three.hs
@@ -19,21 +19,27 @@ getResult :: String -> IO (Int, Int)
 getResult path = do
     ls <- lines <$> readFile path
     let lineData = concatLineData $ zipWith (curry parseLineData) ls [0..] --map parseLineData $ zip ls [0..]
+
     let res1 = sumNums $ filterNums lineData 
     let res2 = sumGearRatios $ getPartNumbers lineData
+
     return (res1, res2)
 
-getBounds:: SCoords -> [(Int, Int)]
-getBounds (SCoords s (x, y)) = [(x', y') |  x' <- [x-1..x + length s], y' <- [y-1..y+1]]
+-- #region part 1
 
 sumNums :: Numbers -> Int
 sumNums = sum . map ((\n -> read n :: Int) . str)
+
 
 filterNums :: (Numbers, Symbols) -> [SCoords]
 filterNums (numbers, symbols) = filter symbolInBounds numbers
     where
         symbolInBounds :: SCoords -> Bool
         symbolInBounds ns = (not . null) $ map xy symbols `intersect` getBounds ns
+
+-- #endregion
+
+-- #region part 2
 
 getGearRatios :: (Int, Int) -> Numbers -> Maybe (SCoords, SCoords)
 getGearRatios gearCoords numbers =
@@ -56,10 +62,19 @@ getPartNumbers (numbers, symbols) = foldr f [] gearCoords
             where
                 partNumbers = getGearRatios gearCoord numbers
 
+
 sumGearRatios:: [(SCoords, SCoords)] -> Int
 sumGearRatios nums = sum $ map f nums
     where
         f (SCoords n1 _, SCoords n2 _) = (read n1 :: Int) * (read n2 :: Int)
+
+-- #endregion
+
+-- #region shared
+
+getBounds:: SCoords -> [(Int, Int)]
+getBounds (SCoords s (x, y)) = [(x', y') |  x' <- [x-1..x + length s], y' <- [y-1..y+1]]
+
 
 parseLineData :: (String, Int) -> (Numbers, Symbols)
 parseLineData (s, y') = (numberCoords, symbolsAt)
@@ -72,7 +87,10 @@ parseLineData (s, y') = (numberCoords, symbolsAt)
         numberCoords = map (\(a, x') -> SCoords a (x', y')) numbers
         symbolsAt = map (\(a, x') -> SCoords a (x', y')) symbols
 
+
 concatLineData :: [(Numbers, Symbols)] -> (Numbers, Symbols)
 concatLineData = foldr f ([], [])
     where
         f (nums, symbolsAt) (nums', symbolsAt') = (nums' ++ nums, symbolsAt' ++ symbolsAt)
+
+-- #endregion
