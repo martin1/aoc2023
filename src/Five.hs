3e69bb4309b1a9da0531797220807935eee72080
Martin Kurgi <martinkurgi@gmail.com>
2024-04-16 12:44:57 +0300

    getMaps

--- src/Five.hs
+++ src/Five.hs
@@ -2,6 +2,7 @@ module Five (getResult) where
 import Data.Char (isDigit)
 import Data.List (isPrefixOf, elemIndex)
 import Data.Maybe (fromJust, isNothing, fromMaybe)
+import Data.List.Split (splitOn)
 
 
 data MapRange = MapRange {
@@ -50,3 +51,51 @@ getRangeValue range n =
         index = elemIndex n sourceList
         destList = [(destStart range) .. (destStart range + len range - 1)]
 
+getMaps :: [String] -> [(String, [MapRange])]
+getMaps ls = map f mapData
+    where
+        mapData :: [[String]]
+        mapData = splitOn [""] ls
+        
+        f :: [String] -> (String, [MapRange])
+        f [] = ("", [])
+        f (x:xs) = (x, map getMapRange xs)
+
+
+testlines :: [String]
+testlines = [
+    "seeds: 79 14 55 13",
+    "",
+    "seed-to-soil map:",
+    "50 98 2",
+    "52 50 48",
+    "",
+    "soil-to-fertilizer map:",
+    "0 15 37",
+    "37 52 2",
+    "39 0 15",
+    "",
+    "fertilizer-to-water map:",
+    "49 53 8",
+    "0 11 42",
+    "42 0 7",
+    "57 7 4",
+    "",
+    "water-to-light map:",
+    "88 18 7",
+    "18 25 70",
+    "",
+    "light-to-temperature map:",
+    "45 77 23",
+    "81 45 19",
+    "68 64 13",
+    "",
+    "temperature-to-humidity map:",
+    "0 69 1",
+    "1 0 69",
+    "",
+    "humidity-to-location map:",
+    "60 56 37",
+    "56 93 4",
+    ""
+    ]
