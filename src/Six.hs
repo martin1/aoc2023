19e3c91db860e5ac910d7377a2a2f463af4e2cf5
Martin Kurgi <martinkurgi@gmail.com>
2024-04-24 22:34:18 +0300

    wip

--- src/Six.hs
+++ src/Six.hs
@@ -1,5 +1,23 @@
 module Six(dayResult) where
 import Types (DayResult (..))
+import Lib (getNumbers)
 
 dayResult :: DayResult
 dayResult = DayResult 6 (\_ -> return (0, 0))
+
+data Race = Race {
+    time:: Int,
+    distance :: Int
+} deriving (Eq, Show)
+
+getRaces :: (String, String) -> [Race]
+getRaces (times, dists) = [Race t d | (t, d) <- zip ts ds]
+    where
+        ts = getNumbers times
+        ds = getNumbers dists
+
+ls :: [String]
+ls = [
+    "Time:      7  15   30",
+    "Distance:  9  40  200"
+    ]
