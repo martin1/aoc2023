b17559058b655ff65530fb73a83c56b877c4113c
Martin Kurgi <martinkurgi@gmail.com>
2024-08-31 10:46:19 +0300

    Refactored tests (two file test improvements)

--- src/Eight.hs
+++ src/Eight.hs
@@ -1,26 +1,44 @@
-module Eight(dayResult) where
-import Types (DayResult(..))
+module Eight(dayResult, dayResults) where
+import Types (DayResult(..), DayResults (..))
 import Text.Parsec ( char, letter, string, many1, parse)
 import Text.Parsec.String ( Parser )
 import Data.Maybe (fromJust)
 import Data.List (unfoldr)
 import qualified Data.Map.Strict as Map
 
+dayResults :: DayResults
+dayResults = DayResults getResult1 getResult2
+
+getResult1 :: String -> IO Int
+getResult1 path = do
+  (dict, moves) <- getData path
+  return $ moveCount moves dict
+
+getResult2 :: String -> IO Int
+getResult2 path = do
+  --(dict, moves) <- getData path
+  return 0
+
 dayResult :: DayResult
 dayResult = DayResult 8 getResults
 
 type Dictionary = Map.Map String (String, String)
+type Moves = [(String, String) -> String]
 
-getResults :: String -> IO (Int, Int)
-getResults path = do
+getData :: String -> IO (Dictionary, Moves)
+getData path = do
   ls <- lines <$> readFile path
   let moveStr = head ls
   let dataStrs = filter (not . null) $ tail ls
   let dict = Map.fromList $ map parseLine dataStrs
-  let moves = getMoves $ cycle moveStr
+  return (dict, getMoves $ cycle moveStr)
+
+getResults :: String -> IO (Int, Int)
+getResults path = do
+  (dict, moves) <- getData path
   return (moveCount moves dict, 0)
 
-moveCount :: [(String, String) -> String] -> Dictionary -> Int
+moveCount :: Moves -> Dictionary -> Int
 moveCount moves dict = length $ unfoldr f (head $ Map.keys dict, moves)
   where
     f (_, []) = Nothing
--- test/Spec.hs
+++ test/Spec.hs
@@ -5,46 +5,58 @@ import Four(dayResult)
 import Five(dayResult)
 import Six(dayResult)
 import Seven(dayResult)
-import Eight(dayResult)
-import Test.Hspec ( hspec, describe, it, shouldReturn, shouldBe, runIO )
+import Eight(dayResults)
+import Test.Hspec ( hspec, describe, it, shouldBe, runIO )
 import Types (DayResult(..), DayResults(..))
 import Text.Printf (printf)
 
 main :: IO ()
 main = do
-    testOne
-    mapM_ makeTest testData
+    mapM_ makeOneFileTest testData1
+    mapM_ makeTwoFileTest testData2
 
 -- test data - name, dayResult, expected1, expected2
-data TestData = TestData String DayResult Int Int
+data TestData1 = TestData1 String DayResult Int Int
+-- test data - name, dayNo, dayResults, expected1, expected2 - for two input files
+data TestData2 = TestData2 Int String DayResults Int Int
 
-testData :: [TestData]
-testData = [
-    TestData "Two" Two.dayResult 8 2286,
-    TestData "Three" Three.dayResult 4361 467835,
-    TestData "Four" Four.dayResult 13 30,
-    TestData "Five" Five.dayResult 35 46,
-    TestData "Six" Six.dayResult 288 71503,
-    TestData "Seven" Seven.dayResult 6440 5905,
-    TestData "Eight" Eight.dayResult 6 0
+testData1 :: [TestData1]
+testData1 = [
+    TestData1 "Two" Two.dayResult 8 2286,
+    TestData1 "Three" Three.dayResult 4361 467835,
+    TestData1 "Four" Four.dayResult 13 30,
+    TestData1 "Five" Five.dayResult 35 46,
+    TestData1 "Six" Six.dayResult 288 71503,
+    TestData1 "Seven" Seven.dayResult 6440 5905
     ]
 
-makeTest :: TestData -> IO ()
-makeTest (TestData testName dayRes expected1 expected2) = hspec $ do
+testData2 :: [TestData2]
+testData2 = [
+    TestData2 1 "One" One.dayResults 142 281,
+    TestData2 8 "Eight" Eight.dayResults 6 0
+    ]
+
+makeOneFileTest :: TestData1 -> IO ()
+makeOneFileTest (TestData1 testName dayRes expected1 expected2) = hspec $ do
     describe testName $ do
         let inputPath = printf "test/input/%d.txt" (dayNo dayRes)
         (res1, res2) <- runIO $ getRes dayRes inputPath
-        
+
         it "res1" $ do
             res1 `shouldBe` expected1
         it "res2" $ do
             res2 `shouldBe` expected2
 
-testOne :: IO ()
-testOne = hspec $ do
-    describe "One" $ do
-        let dayRes = One.dayResults
+makeTwoFileTest :: TestData2 -> IO()
+makeTwoFileTest (TestData2 dayNo testName dayRes expected1 expected2) = hspec $ do
+    describe testName $ do
+        let inputPath1 = printf "test/input/%d_1.txt" dayNo
+        let inputPath2 = printf "test/input/%d_2.txt" dayNo
+
+        res1 <- runIO $ getRes1 dayRes inputPath1
+        res2 <- runIO $ getRes2 dayRes inputPath2
+
         it "res1" $ do
-            getRes1 dayRes "test/input/1_1.txt" `shouldReturn` 142
+            res1 `shouldBe` expected1
         it "res2" $ do
-            getRes2 dayRes "test/input/1_2.txt" `shouldReturn` 281
+            res2 `shouldBe` expected2
